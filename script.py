d79e7e89622fb9aff37a2b5ea2930a7dde7d11db
DoYouNotice <94945611+DoYouNotice@users.noreply.github.com>
2023-05-27 11:45:35 +0200

    Update script.py

--- script.py
+++ script.py
@@ -2,16 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import locale
-from decimal import Decimal
 import json
+from decimal import Decimal
 
-# Set the appropriate locale for number formatting
+
 locale.setlocale(locale.LC_ALL, '')
 
 graphics_cards = {
     "GeForce RTX 3060 Ti": "GeForce+RTX+fuer+Gaming/RTX+3060+Ti",
     "GeForce RTX 4090": "GeForce+RTX+fuer+Gaming/RTX+4090",
-    # Add more graphics cards here...
+    "Radeon RX 7900 XTX": "Radeon+RX+Serie/RX+7900+XTX",
+    "GeForce RTX 4080": "GeForce+RTX+fuer+Gaming/RTX+4080",
+    "Radeon RX 7900 XT": "Radeon+RX+Serie/RX+7900+XT",
+    "Radeon RX 6950 XT": "Radeon+RX+Serie/RX+6950+XT",
+    "GeForce RTX 4070 Ti": "GeForce+RTX+fuer+Gaming/RTX+4070+Ti",
+    "GeForce RTX 3090 Ti": "GeForce+RTX+fuer+Gaming/RTX+3090+Ti",
+    "Radeon RX 6900 XT": "Radeon+RX+Serie/RX+6900+XT",
+    "GeForce RTX 3090": "GeForce+RTX+fuer+Gaming/RTX+3090",
+    "Radeon RX 6800 XT": "Radeon+RX+Serie/RX+6800+XT",
+    "GeForce RTX 3080 Ti": "GeForce+RTX+fuer+Gaming/RTX+3080+Ti",
+    "GeForce RTX 3080 12GB": "GeForce+RTX+fuer+Gaming/RTX+3080+12GB",
+    "GeForce RTX 4070": "GeForce+RTX+fuer+Gaming/RTX+4070",
+    "GeForce RTX 3080": "GeForce+RTX+fuer+Gaming/RTX+3080",
+    "Radeon RX 6800": "Radeon+RX+Serie/RX+6800",
+    "GeForce RTX 3070 Ti": "GeForce+RTX+fuer+Gaming/RTX+3070+Ti",
+    "Radeon RX 6750 XT": "Radeon+RX+Serie/RX+6750+XT",
+    "Radeon RX 6700 XT": "Radeon+RX+Serie/RX+6700+XT",
+    "GeForce RTX 3060": "GeForce+RTX+fuer+Gaming/RTX+3060",
+    "Radeon RX 6600 XT": "Radeon+RX+Serie/RX+6600+XT",
+    "Radeon RX 6500 XT": "Radeon+RX+Serie/RX+6500+XT",
+    "Radeon RX 7600": "Radeon+RX+Serie/RX+7600",
+    "GeForce RTX 4060 Ti": "GeForce+RTX+fuer+Gaming/RTX+4060+TI",
+    "Radeon RX 6650 XT": "Radeon+RX+Serie/RX+6650+XT",    
 }
 
 url = "https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html"
@@ -20,83 +42,34 @@ url = "https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html"
 response = requests.get(url)
 
 # Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.content, "html.parser")
 
-# Find all table rows within the desired section
-table_rows = soup.select('#slice-container-L8SfcAV5o28ayEFjzq6cfX-table-10 > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr')
+# Find the table rows containing graphics card information
+rows = soup.find_all("tr")
 
-# Create a dictionary to store the FPS data
-fps_data = {}
-
-# Loop through the table rows and extract FPS and name values
-for row in table_rows:
-    fps_percentage = row.select_one('td:nth-child(2)').text.strip()
-    fps_match = re.findall(r'([\d.]+)fps', fps_percentage)  # Extract the FPS value using regex
-
-    if fps_match:  # Check if FPS value is found
-        fps = Decimal(fps_match[0])  # Get the first element from the list and convert to Decimal
-    else:
-        fps = None  # Set fps as None if no value is found
-
-    name = row.select_one('td:nth-child(1) > strong:nth-child(1) > a:nth-child(1)').text.strip()
-
-    # Store the FPS data in the dictionary
-    fps_data[name] = fps
-
-base_url = "https://www.mindfactory.de/Hardware/Grafikkarten+(VGA)/"
-cheapest_prices = {}
+# Create a dictionary to store the cost per frame for each graphics card
 cost_per_frame = {}
 
-for card, card_url in graphics_cards.items():
-    url = base_url + card_url + ".html/listing_sort/6"
+# Iterate over the rows, skipping the header row
+for row in rows[1:]:
+    # Find the columns in the row
+    columns = row.find_all("td")
 
-    # Send a GET request to the URL
-    response = requests.get(url)
+    # Extract the graphics card name and cost per frame
+    name = columns[0].text.strip()
+    cost = columns[2].text.strip().replace("$", "").replace(",", "")
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, "html.parser")
+    # Convert the cost to a Decimal object
+    cost = Decimal(cost)
 
-    # Find the desired element using CSS selector
-    element = soup.select_one("div.p:nth-child(6) > div:nth-child(1) > div:nth-child(9)")
+    # Check if the graphics card is in our dictionary
+    if name in graphics_cards:
+        # Add the graphics card and its cost per frame to the dictionary
+        cost_per_frame[name] = cost
 
-    if element:
-        price_text = element.text.strip()
-        price_text = re.sub(r"[^\d.,]", "", price_text)  # Remove non-digit characters
-        price_text = price_text.replace(".", "").replace(",", ".")  # Replace decimal separator
-        price = Decimal(price_text.strip())
-        cheapest_prices[card] = price
-        cost_per_frame[card] = price / (fps_data.get(card) or 1)  # Assign 1 if fps_data[card] is None
+# Convert Decimal values to float before serializing
+serialized_results = {card: float(cost) for card, cost in cost_per_frame.items()}
 
-# Sort the graphics cards by cost per frame (lowest to highest)
-sorted_cards = sorted(cost_per_frame.items(), key=lambda x: x[1])
-
-# Store the results in a JSON file
-results = []
-for card, cost in sorted_cards:
-    price = cheapest_prices[card]
-    formatted_price = locale.format_string("%.2f", price, grouping=True)
-    cost_per_frame_value = locale.format_string("%.2f", cost)
-    fps_value = fps_data.get(card, "N/A")
-    
-    result = {
-        "card": card,
-        "cheapest_price": formatted_price,
-        "cost_per_frame": cost_per_frame_value,
-        "fps": fps_value
-    }
-    results.append(result)
-
-with open("results.json", "w") as file:
-    json.dump(results, file)
-
-# Print the results to the console
-for card, cost in sorted_cards:
-    price = cheapest_prices[card]
-    formatted_price = locale.format_string("%.2f", price, grouping=True)
-    cost_per_frame_value = locale.format_string("%.2f", cost)
-    fps_value = fps_data.get(card, "N/A")
-    print(f"{card}:")
-    print(f"Cheapest price: {formatted_price} €")
-    print(f"Cost per frame: {cost_per_frame_value} € per frame")
-    print(f"FPS: {fps_value} (Ultra 1080p)")
-    print()
+# Save results to JSON file
+with open('results.json', 'w') as file:
+    json.dump(serialized_results, file)
