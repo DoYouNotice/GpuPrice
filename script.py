bc196de88534f8fc36f91d95d4e5c97a5a79292e
DoYouNotice <94945611+DoYouNotice@users.noreply.github.com>
2023-05-27 12:04:29 +0200

    Update script.py

--- script.py
+++ script.py
@@ -4,8 +4,8 @@ import re
 import locale
 import json
 from decimal import Decimal
-import decimal
 
+# Set the appropriate locale for number formatting
 locale.setlocale(locale.LC_ALL, '')
 
 graphics_cards = {
@@ -19,7 +19,7 @@ url = "https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html"
 response = requests.get(url)
 
 # Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(response.content, 'html.parser')
 
 # Find the table rows containing graphics card information
 rows = soup.find_all("tr")
@@ -32,31 +32,27 @@ for row in rows[1:]:
     # Find the columns in the row
     columns = row.find_all("td")
 
+    # Extract the graphics card name and cost per frame
+    name = columns[0].text.strip()
+    cost = columns[2].text.strip().replace("$", "").replace(",", "")
+
     try:
-        # Extract the graphics card name and cost per frame
-        name = columns[0].text.strip()
-        cost = columns[2].text.strip().replace("$", "").replace(",", "")
+        # Convert the cost to a Decimal object
+        cost = Decimal(cost)
+    except (Decimal.InvalidOperation, TypeError):
+        # Handle conversion errors by setting a default cost value
+        cost = Decimal(0)
 
-        try:
-            # Convert the cost to a Decimal object
-            cost = Decimal(cost)
-        except (decimal.InvalidOperation, TypeError):
-            # Handle conversion errors by setting a default cost value
-            cost = Decimal(0)
-
-        # Check if the graphics card is in our dictionary
-        if name in graphics_cards:
-            # Add the graphics card and its cost per frame to the dictionary
-            cost_per_frame[name] = cost
-
-    except IndexError:
-        # Handle the IndexError and print the row for debugging
-        print("Error: Row skipped due to IndexError")
-        print("Row contents:", [column.text.strip() for column in columns])
-
-# Convert Decimal values to float before serializing
-serialized_results = {card: float(cost) for card, cost in cost_per_frame.items()}
+    # Check if the graphics card is in our dictionary
+    if name in graphics_cards:
+        # Add the graphics card and its cost per frame to the dictionary
+        cost_per_frame[name] = float(cost)
 
 # Save results to JSON file
 with open('results.json', 'w') as file:
-    json.dump(serialized_results, file)
+    json.dump(cost_per_frame, file)
+
+# Commit the JSON file to your GitHub repository
+commit_message = "Update results.json"
+commit_command = f'git add results.json && git commit -m "{commit_message}" && git push'
+os.system(commit_command)
