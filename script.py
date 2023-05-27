47e8c866de35b6ec20b3f8442dcff1ef144e0170
DoYouNotice <94945611+DoYouNotice@users.noreply.github.com>
2023-05-27 12:11:34 +0200

    Update script.py

--- script.py
+++ script.py
@@ -15,44 +15,92 @@ graphics_cards = {
 
 url = "https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html"
 
-# Send a GET request to the URL
-response = requests.get(url)
+# Function to scrape the data and return it as a JSON string
+def scrape_graphics_card_data():
+    # Send a GET request to the URL
+    response = requests.get(url)
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find the table rows containing graphics card information
-rows = soup.find_all("tr")
+    # Find the table rows containing graphics card information
+    rows = soup.find_all("tr")
 
-# Create a dictionary to store the cost per frame for each graphics card
-cost_per_frame = {}
+    # Create a dictionary to store the cost per frame for each graphics card
+    cost_per_frame = {}
 
-# Iterate over the rows, skipping the header row
-for row in rows[1:]:
-    # Find the columns in the row
-    columns = row.find_all("td")
+    # Iterate over the rows, skipping the header row
+    for row in rows[1:]:
+        # Find the columns in the row
+        columns = row.find_all("td")
 
-    # Extract the graphics card name and cost per frame
-    name = columns[0].text.strip()
-    cost = columns[2].text.strip().replace("$", "").replace(",", "")
+        # Extract the graphics card name and cost per frame
+        name = columns[0].text.strip()
+        cost = columns[2].text.strip().replace("$", "").replace(",", "")
 
-    try:
-        # Convert the cost to a Decimal object
-        cost = Decimal(cost)
-    except (Decimal.InvalidOperation, TypeError):
-        # Handle conversion errors by setting a default cost value
-        cost = Decimal(0)
+        try:
+            # Convert the cost to a Decimal object
+            cost = Decimal(cost)
+        except (Decimal.InvalidOperation, TypeError):
+            # Handle conversion errors by setting a default cost value
+            cost = Decimal(0)
 
-    # Check if the graphics card is in our dictionary
-    if name in graphics_cards:
-        # Add the graphics card and its cost per frame to the dictionary
-        cost_per_frame[name] = float(cost)
+        # Check if the graphics card is in our dictionary
+        if name in graphics_cards:
+            # Add the graphics card and its cost per frame to the dictionary
+            cost_per_frame[name] = float(cost)
 
-# Save results to JSON file
-with open('results.json', 'w') as file:
-    json.dump(cost_per_frame, file)
+    # Convert the data to JSON format
+    json_data = json.dumps(cost_per_frame)
+    
+    return json_data
 
-# Commit the JSON file to your GitHub repository
-commit_message = "Update results.json"
-commit_command = f'git add results.json && git commit -m "{commit_message}" && git push'
-os.system(commit_command)
+# Generate the HTML code with JavaScript to scrape the data when the page is loaded
+html_code = f"""
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Graphics Card Data</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script>
+    $(document).ready(function() {{
+      // Function to handle the click event
+      function handleClick() {{
+        $.ajax({{
+          type: "GET",
+          url: "{url}",
+          success: function(response) {{
+            var data = $(response).find("table tr:has(td)").map(function(i, v) {{
+              var $td = $(this).find("td");
+              var name = $td.eq(0).text().trim();
+              var cost = parseFloat($td.eq(2).text().trim().replace("$", "").replace(",", ""));
+              return {{ name: name, cost: cost }};
+            }}).get();
+            
+            var html = "";
+            for (var i = 0; i < data.length; i++) {{
+              var card = data[i];
+              html += "<p>" + card.name + ": Cost per frame - " + card.cost + "</p>";
+            }}
+            
+            $("#data-container").html(html);
+          }}
+        }});
+      }}
+      
+      // Bind the click event to the button
+      $("#scrape-button").click(handleClick);
+    }});
+  </script>
+</head>
+<body>
+  <h1>Graphics Card Data</h1>
+  <button id="scrape-button">Scrape Data</button>
+  <div id="data-container"></div>
+</body>
+</html>
+"""
+
+# Save the HTML code to a file
+with open('index.html', 'w') as file:
+    file.write(html_code)
