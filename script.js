568174314b67e752a2f3f89d22f7f6d24dfbd2c2
DoYouNotice <94945611+DoYouNotice@users.noreply.github.com>
2023-05-27 10:40:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $.get("results.json", function(data) {
+        // Parse the JSON data
+        var results = JSON.parse(data);
+        
+        // Update the HTML page with the results
+        var resultsDiv = $("#results");
+        for (var card in results) {
+            var cost = results[card].cost_per_frame;
+            var fps = results[card].fps;
+            
+            var cardDiv = $("<div>");
+            cardDiv.append("<h2>" + card + "</h2>");
+            cardDiv.append("<p>Cost per frame: " + cost + "</p>");
+            cardDiv.append("<p>FPS: " + fps + "</p>");
+            
+            resultsDiv.append(cardDiv);
+        }
+    });
+});
